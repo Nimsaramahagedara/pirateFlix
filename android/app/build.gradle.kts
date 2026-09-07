5f0fbac649239a208d760571e4691095f51019dd
nimsaramahagedara <nimsara@blocknetlabs.com>
2026-09-07 23:00:16 +0530

    Add GitHub Actions workflow to build Android TV release APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
